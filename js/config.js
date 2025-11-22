0a36e6900d230bd53b796cc6a5bd2074d44f7baf
大淘客网 <dataokewang@gmail.com>
2025-11-22 20:01:32 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ const SITE_CONFIG = {
 // API站点配置
 const API_SITES = {
     dyttzy: {
-        api: 'http://caiji.dyttzyapi.com',
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod/',
         name: '电影天堂资源',
         detail: 'http://caiji.dyttzyapi.com',
     },
